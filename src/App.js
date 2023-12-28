ed953dd5be78961d5132625a30ac3af2bb174a4c
_woo_s.j <sjwoo1999@korea.ac.kr>
2023-12-28 11:35:51 +0900

    23.12.28

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="personal-page">
+        <div className="profile">
+          <h1>우성종</h1>
+          <br />
+          <img
+            src="https://i.imgur.com/vwv2YNG.jpeg"
+            alt="이미지"
+            width="100"
+            height="130"
+            style={{ borderRadius: 50 }}
+          />
+        </div>
+        <br />
+        <div className="MBTI">
+          <button
+            style={{ background: "#99CCFF" }}
+            onClick={() =>
+              window.open(
+                "https://www.16personalities.com/ko/%EC%84%B1%EA%B2%A9%EC%9C%A0%ED%98%95-estj"
+              )
+            }
+          >
+            MBTI : ESTJ
+          </button>
+        </div>
+        <br />
+        <div className="blog">
+          <button
+            style={{ background: "#FFDD00" }}
+            onClick={() => window.open("https://sjwoo1999.tistory.com/")}
+          >
+            티스토리
+          </button>
+        </div>
+        <br />
+        <div>
+          안녕하세요! 우성종입니다. <br />
+          내배캠을 통해 Node.js 열심히 배워보겠습니다~
+        </div>
+        <br />
+      </div>
+    );
+  }
 }
 
 export default App;
